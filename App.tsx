d843f38e0775b27c65c41ee24197a14349633517
simransayyad <simransayyad22@gmail.com>
2023-07-07 17:57:45 +0530

    changes

---
+++ App.tsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import DataStreamer, { ServerRespond } from './DataStreamer';
+import Graph from './Graph';
+import './App.css';
+
+interface IState {
+  data: ServerRespond[],
+  showGraph: boolean,
+}
+
+class App extends Component<{}, IState> {
+  state: { data: never[]; showGraph: boolean; };
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      data: [],
+      showGraph: false,
+    };
+  }
+
+  renderGraph() {
+    if (this.state.showGraph) {
+      return (<Graph data={this.state.data}/>)
+    }
+  }
+
+  getDataFromServer() {
+    let x = 0;
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({
+          data: serverResponds,
+          showGraph: true,
+        });
+      });
+      x++;
+      if (x > 1000) {
+        clearInterval(interval);
+      }
+    }, 100);
+  }
+  setState(arg0: { data: ServerRespond[]; showGraph: boolean; }) {
+    throw new Error('Method not implemented.');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Bank Merge & Co Task 3
+        </header>
+        <div className="App-content">
+          <button className="btn btn-primary Stream-button" onClick={() => {this.getDataFromServer()}}>Start Streaming Data</button>
+          <div className="Graph">
+            {this.renderGraph()}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
